274244b2bfb57683d24efeadec2c8407360dd18f
FRESHVIEW\dannyl <dannyl@campaignmonitor.com>
2018-03-16 19:30:10 -0700

    use internal process variable

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(module.filename);
-const env = ENV['environment'] || 'development';
+const env = process.env.NODE_ENV || 'development';
 if (env === 'production') {
   const config = require('../config/config');
 }
